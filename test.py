89186fedebabe074b365be6dcd01f2749537e7c0
Ty Day <tyrday@gmail.com>
2018-11-10 11:44:37 -0500

    Change  Timezone back to UTC

---
+++ test.py
@@ -0,0 +1,10 @@
+#!python2
+
+# holy shit. you can use py to run python and it picks the correct version
+# if you use that # exclamation point to start out your script
+
+# Don't understand why vscode indicates a python version at the bottom
+# also, I need to install a 64 bit version of python
+import sys
+curv = sys.version_info
+print(curv)
