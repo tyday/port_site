415c3028b88538fb008180f62dbb955e20a1862d
Ty Day <tyrday@gmail.com>
2018-11-10 22:33:35 -0500

    Secure settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#bysmmosctn2*(*-q33s3d3i^cjg&#&9u2hr4d1^jnv69my5ye'
+# SECRET_KEY = '#bysmmosctn2*(*-q33s3d3i^cjg&#&9u2hr4d1^jnv69my5ye'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '#bysmmosctn2*(*-q33s3d3i^cjg&#&9u2hr4d1^jnv69my5ye')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['142.93.201.4',]
 
