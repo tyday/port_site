1d9c8ffba1dc813ccda5080cdd7a325bcbd0dba2
Ty Day <tyrday@gmail.com>
2018-11-13 00:07:25 -0500

    reversed blog flow. removed body padding

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.exclude(published_date__isnull=True).order_by('published_date')
+    posts = reversed(Post.objects.exclude(published_date__isnull=True).order_by('published_date'))
     return render(request, 'blog/post_list.html', {'posts': posts})
