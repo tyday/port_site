30cf2416804bee4cd3e8bdef010ad88adba71e61
Ty Day <tyrday@gmail.com>
2018-11-13 13:10:26 -0500

    add post_detail.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from .models import Post
 
@@ -6,3 +6,7 @@ from .models import Post
 def post_list(request):
     posts = reversed(Post.objects.exclude(published_date__isnull=True).order_by('published_date'))
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
