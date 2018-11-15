f7ba84d9aa7850b4f5a225a95b22d483c77fea2d
Ty Day <tyrday@gmail.com>
2018-11-14 22:35:47 -0500

    Add classes to form elements and clear button

--- mysite/sendemail/forms.py
+++ mysite/sendemail/forms.py
@@ -2,6 +2,13 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    from_email = forms.EmailField(widget=forms.TextInput(
+        attrs={'class' : 'email-from',
+        'placeholder':'your email'}),
+        required=True)
+    subject = forms.CharField(widget=forms.TextInput(
+        attrs={'class' : 'email-subject',
+                'placeholder':'Subject'}), 
+        required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'id' : 'email-message'}), required=True)
+    
