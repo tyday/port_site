913ec3bf260c97e15fd32808fc424bc646c6d712
Ty Day <tyrday@gmail.com>
2018-11-20 00:00:03 -0500

    Add styling to blog and photo to post Model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    photo = models.CharField(max_length=200, blank=True,null=True)
 
     def publish(self):
         self.puplished_date = timezone.now()
