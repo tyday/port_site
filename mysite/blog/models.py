4d1aa29065f0fa47dc6f26b070463850fae1fa1f
Ty Day <tyrday@gmail.com>
2018-12-21 13:46:09 -0500

    Add MEDIA_ROOT support. Change Post model to use ImageField instead of Charfield to store photos

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -10,7 +10,8 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    photo = models.CharField(max_length=200, blank=True,null=True)
+    photo = models.ImageField(null=True,blank=True)
+    photo_alt_text = models.CharField(max_length=200, blank=True,null=True)
 
     def publish(self):
         self.puplished_date = timezone.now()
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -128,8 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# May delete STATIC_ROOT don't know if it's needed
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 # Email handler
 # https://wsvincent.com/django-contact-form/
