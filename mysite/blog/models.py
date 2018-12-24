43e317ddef42b46d942ce40a29751afbc096f70b
Ty Day <tyrday@gmail.com>
2018-12-23 19:01:41 -0500

    add Image and Product to models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 
@@ -18,3 +20,29 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+class Image(models.Model):
+    name = models.CharField(max_length=100)
+    image_file = models.ImageField(null=True,blank=True)
+    alt_text = models.CharField(max_length=100)
+
+    # I'm not decided on this so I'm going to keep it in for now in comment form. I may need it later.
+    # generic relation stuff
+    # https://simpleisbetterthancomplex.com/tutorial/2016/10/13/how-to-use-generic-relations.html
+    # https://docs.djangoproject.com/en/2.1/ref/contrib/contenttypes/#generic-relations
+    # content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # object_id = models.PositiveIntegerField()
+    # content_object = GenericForeignKey()
+
+    def __str__(self):
+        return self.name
+    
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    # the short description is for the front page. I'm debating whether to use a CharField with a max_length rather than a TextField
+    short_description = models.TextField()
+    description = models.TextField()
+    images = models.ManyToManyField(Image)
+    
+    def __str__(self):
+        return self.name
