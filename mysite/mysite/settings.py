ff3d505ba1d5904f4c4e46efcd2441f7f34f4247
Ty Day <tyrday@gmail.com>
2019-02-28 14:12:35 -0500

    convert to Postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': secret_settings.DB_NAME,
+        'USER' : secret_settings.DB_USER,
+        'PASSWORD' : secret_settings.DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
