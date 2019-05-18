e5d1761a9d8807b59ac42c66c626394f94a3d5d5
Ty Day <tyrday@gmail.com>
2019-05-18 16:06:08 -0400

    styling added

--- mysite/weather/src/components/AreaForecastDiscussion.js
+++ mysite/weather/src/components/AreaForecastDiscussion.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "../styles/AreaForecastDiscussion.css"
 
 export default function AreaForecastDiscussion(props) {
     const afdList = props.afd.split("\n")
     // console.log(afdList)
   return (
-    <div>
+    <div className="text-content">
       {afdList.map((value,index)=>{
           return <p key={index}>{value}</p>
       })}
