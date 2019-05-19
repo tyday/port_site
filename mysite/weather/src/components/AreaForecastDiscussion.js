64b35b17bbfea1c6bbabfe988d5dc9e41253a6fa
Ty Day <tyrday@gmail.com>
2019-05-18 21:28:09 -0400

    Area Forecast toggleable

--- mysite/weather/src/components/AreaForecastDiscussion.js
+++ mysite/weather/src/components/AreaForecastDiscussion.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import "../styles/AreaForecastDiscussion.css"
+import React from "react";
+import "../styles/AreaForecastDiscussion.css";
 
 export default function AreaForecastDiscussion(props) {
-    const afdList = props.afd.split("\n")
-    // console.log(afdList)
+  function showAFD(e) {
+    console.log(e.target);
+    var iAm = e.target;
+    var content = iAm.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  }
+  const afdList = props.afd.split("\n");
+  // console.log(afdList)
   return (
     <div className="text-content">
-      {afdList.map((value,index)=>{
-          return <p key={index}>{value}</p>
-      })}
+      <button onClick={showAFD} className="collapsible">
+        Open AFD
+      </button>
+      <div style={{display:"none"}}>
+        {afdList.map((value, index) => {
+          return <p key={index}>{value}</p>;
+        })}
+      </div>
     </div>
-  )
+  );
 }
--- mysite/weather/src/components/Table.js
+++ mysite/weather/src/components/Table.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import key from "weak-key";
+
+
 const Table = ({ data }) =>
   !data ? (
     <p>Nothing to show</p>
