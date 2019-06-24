484ff811929c7c98924ce2a03dccc436fb1b21cc
Ty Day <tyrday@gmail.com>
2019-06-24 19:37:04 -0400

    edit posts/new posts require login

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.http import HttpResponse, HttpResponseRedirect
@@ -40,6 +41,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
 
+@login_required(login_url='/admin/login/')
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -53,6 +55,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required(login_url='/admin/login/')
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
