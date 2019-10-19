02c91add1ac2b615d604aed7e4e36250c2cf96a8
Ty Day <tyrday@gmail.com>
2019-10-18 22:33:41 -0400

    added utility to rotate photos

---
+++ mysite/common/utilities/image_manipulation.py
@@ -0,0 +1,24 @@
+# rotates an image to match it's exif information
+# https://medium.com/@giovanni_cortes/rotate-image-in-django-when-saved-in-a-model-8fd98aac8f2a
+
+from PIL import Image, ExifTags
+
+def rotate_image(filepath):
+  try:
+    image = Image.open(filepath)
+    for orientation in ExifTags.TAGS.keys():
+      if ExifTags.TAGS[orientation] == 'Orientation':
+            break
+    exif = dict(image._getexif().items())
+
+    if exif[orientation] == 3:
+        image = image.rotate(180, expand=True)
+    elif exif[orientation] == 6:
+        image = image.rotate(270, expand=True)
+    elif exif[orientation] == 8:
+        image = image.rotate(90, expand=True)
+    image.save(filepath)
+    image.close()
+  except (AttributeError, KeyError, IndexError):
+    # cases: image don't have getexif
+    pass
