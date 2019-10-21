a4e1ff8fabaca0d00dd42313b082dadfaa91a665
Ty Day <tyrday@gmail.com>
2019-10-20 21:51:55 -0400

    made blog and project images progressive

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,9 +1,15 @@
+import os
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from common.utilities.image_manipulation import rotate_image, create_responsive_images
 
 # Create your models here.
 
@@ -18,6 +24,7 @@ class Image(models.Model):
     name = models.CharField(max_length=100)
     image_file = models.ImageField(null=True,blank=True)
     alt_text = models.CharField(max_length=100)
+    source_set = models.TextField(blank=True, null=True)
 
     # I'm not decided on this so I'm going to keep it in for now in comment form. I may need it later.
     # generic relation stuff
@@ -78,3 +85,19 @@ class Project(models.Model):
     
     def __str__(self):
         return self.name
+
+@receiver(post_save, sender=Image, dispatch_uid="update_image")
+def update_image(sender, instance, **kwargs):
+    if instance.image_file:
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        fullpath = BASE_DIR + instance.image_file.url
+        rotate_image(fullpath)
+
+        # Creates responsive images
+        # then return a string consisting of the srcset
+        srcset = create_responsive_images(fullpath, instance.image_file.url)
+        instance.source_set = srcset
+
+        post_save.disconnect(update_image, sender=Image, dispatch_uid="update_image")
+        instance.save()
+        post_save.connect(update_image, sender=Image, dispatch_uid="update_image")
--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from common.utilities.image_manipulation import rotate_image, create_responsive_images
