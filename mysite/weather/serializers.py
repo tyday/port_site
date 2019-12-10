e2f7d7d3aaf140dd11482a0c128a6788a81067f3
Ty Day <tyrday@gmail.com>
2019-12-09 20:35:52 -0500

    add more categories to weather api

--- mysite/weather/serializers.py
+++ mysite/weather/serializers.py
@@ -5,5 +5,15 @@ class ObservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Observation
         fields = [
-             'observer','observation_date', 'perceived_outdoor_temperature', 'perceived_outdoor_humidity'
+             'observer',
+             'observation_date',
+             'perceived_outdoor_temperature',
+             'perceived_outdoor_humidity',
+             'observed_outdoor_temperature',
+             'observed_outdoor_humidity',
+             'observed_pressure_millibars',
+             'surface_wind_direction',
+             'surface_wind_speed',
+             'cloud_observation',
+             'cloud_coverage',
             ]
